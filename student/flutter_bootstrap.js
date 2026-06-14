9efa197326f461488e0d0e5da87a540ffa719a41
viru0cool <viru0cool@users.noreply.github.com>
2026-06-14 11:00:36 +0000

    Improve web login submit flow viru0cool/SAS_PROJECT@e5ba25b69344b9a28891c62835723ca5193b22ab

--- student/flutter_bootstrap.js
+++ student/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "712494926" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1814137063" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
