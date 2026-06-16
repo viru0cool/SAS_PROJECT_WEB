207738c1bf21c8f18a7a60adc5323b05d4ed177d
viru0cool <viru0cool@users.noreply.github.com>
2026-06-16 08:47:18 +0000

    Limit public website modes viru0cool/SAS_PROJECT@9ed335bdb570818f579495f29db612db00d33c4b

--- parent/flutter_bootstrap.js
+++ parent/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2024701616" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3894412551" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
