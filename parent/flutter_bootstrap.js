3b7e6500ee07ab598e0edda61d66b810cee382ff
viru0cool <viru0cool@users.noreply.github.com>
2026-06-16 07:08:05 +0000

    Add fee quick file launchers viru0cool/SAS_PROJECT@6b537129f72fc9a36e3a774eb179bbce83957f22

--- parent/flutter_bootstrap.js
+++ parent/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "876955841" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3233811146" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
